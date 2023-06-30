ada085b44af80464c7a32831ef25cfa38103a5f4
Hegnon <hegnondutra@gmail.com>
2023-06-30 17:03:35 -0300

    feat: cadastro autorrefac

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
 gem "rspec-rails"
 
+gem "cpf_cnpj"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- app/models/autor.rb
+++ app/models/autor.rb
@@ -1,2 +1,10 @@
 class Autor < ApplicationRecord
+    validates :nome, presence: true
+    validates :nome, length: { minimum: 3, maximum: 60 }
+    validate :cpf_is_valid?
+
+    private
+    def cpf_is_valid?
+      errors.add(:cpf, "CPF inválido! Por favor, verificar os dados.") unless CPF.valid?(cpf)
+    end
 end
