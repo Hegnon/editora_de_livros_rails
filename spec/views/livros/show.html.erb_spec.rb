449c94c62b90306d6f95deb16b18c1cda5b487dd
Hegnon <hegnondutra@gmail.com>
2023-07-09 18:26:31 -0300

    feat: finish test livro

--- spec/views/livros/show.html.erb_spec.rb
+++ spec/views/livros/show.html.erb_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "livros/show", type: :view do
+  let(:autor) {
+    Autor.create(nome: "name", cpf: "12463278722")
+  }
+
   before(:each) do
     assign(:livro, Livro.create!(
-      titulo: "Titulo",
-      autor: nil,
-      publicado: "Publicado",
-      isbn: "Isbn"
+      titulo: "teste",
+      autor: autor,
+      publicado: "01/01/2020",
+      isbn: "9788533302273"
     ))
   end
 
