1ac00e6634f116d2445b57c6fbf2173bb03d4548
Hegnon <hegnondutra@gmail.com>
2023-07-24 07:27:53 -0300

    feat: validação nome e cnpj fornecedores

--- app/models/fornecedor.rb
+++ app/models/fornecedor.rb
@@ -1,2 +1,11 @@
 class Fornecedor < ApplicationRecord
+
+    validates :nome, presence: true
+  validates :nome, length: { minimum: 3, maximum: 60 }
+  validate :cnpj_is_valid?
+
+  private
+  def cnpj_is_valid?
+    errors.add(:cnpj, "CNPJ inválido! Por favor, verifique os dados." ) unless CNPJ.valid?(cnpj)
+  end
 end
