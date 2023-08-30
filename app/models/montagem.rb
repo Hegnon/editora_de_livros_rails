d9e21be1de2420110bc3fd1beeb32b7030dfcb13
Hegnon <hegnondutra@gmail.com>
2023-08-30 07:31:06 -0300

    feat:adicionando varias peças em montagem

--- app/models/montagem.rb
+++ app/models/montagem.rb
@@ -1,5 +1,5 @@
 class Montagem < ApplicationRecord
     belongs_to :livro
-    has_and_belongs_to_many :pecas
+    has_and_belongs_to_many :pecas, join_table: 'montagem_pecas'
 
 end
--- app/models/peca.rb
+++ app/models/peca.rb
@@ -1,5 +1,5 @@
 class Peca < ApplicationRecord
     belongs_to :fornecedor
-    has_and_belongs_to_many :montagems
+    has_and_belongs_to_many :montagems, join_table: 'montagem_pecas'
     #has_many :montagems, through: :montagem_pecas
 end
